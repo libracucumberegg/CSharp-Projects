fd60274e69a0ebadc6c0423e803c4f9709bd6052
libracucumberegg <markus.hansson04@outlook.com>
2023-08-29 12:06:52 -0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Input a number"); // input initial number
+            if (double.TryParse(Console.ReadLine(), out double input)) { // prevents non numeric characters from being used
+                Console.WriteLine(input * 50); // prints input multiplied by 50
+                Console.WriteLine(input + 25); // prints 25 added to input
+                Console.WriteLine(input / 12.5); // prints input divided by 12.5
+                Console.WriteLine(input > 50); // prints if number is > 50
+                Console.WriteLine(input % 7); // prints remainder
+                Console.ReadLine();
+            } else {
+                Console.WriteLine("Please input a valid number"); // if input is not numeric then have the user try another number
+            }
+        }
+    }
+}
