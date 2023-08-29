c8bed4d1a63122cbee36b57fe946b896b42b22b3
libracucumberegg <markus.hansson04@outlook.com>
2023-08-29 15:35:20 -0700

    while loop assignment

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4 {
+    class Program {
+        private static int number = 0;
+
+        static void Main(string[] args) {
+            while (number++ <= 10) { // adds 1 to number and prints to console if the number is less than or equal to 10
+                Console.WriteLine("Number is still less than or equal to 10");
+            }
+
+            do { // adds 1 to number and prints to console what the number currently is until it reaches 20
+                number++;
+                Console.WriteLine("The number is currently equal to " + number);
+            } while (number != 20);
+
+            Console.ReadLine();
+        }
+    }
+}
