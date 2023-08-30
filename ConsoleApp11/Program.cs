3a92d42e12b594440699ccc3ddeef50f233c2f79
libracucumberegg <markus.hansson04@outlook.com>
2023-08-30 14:58:16 -0700

    method assignment

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp11 {
+    class Program {
+        static void Main(string[] args) {
+            Employee employee = new Employee() { firstName = "James", lastName = "Adams" }; // makes new employee
+            
+            employee.SayName();
+            Console.ReadLine(); // lets user read output
+        }
+    }
+
+    public class Person { 
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public void SayName() {
+            Console.WriteLine("Name: " + firstName + lastName);
+        }
+    }
+
+    public class Employee : Person { // inherit person class
+        public int id { get; set; }
+    }
+}
