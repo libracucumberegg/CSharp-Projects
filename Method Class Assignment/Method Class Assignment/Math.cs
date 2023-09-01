669cccd3e9646529c5c8c46f462b45f15d532d7f
libracucumberegg <markus.hansson04@outlook.com>
2023-09-01 16:02:54 -0700

    fixes

--- Method Class Assignment/Method Class Assignment/Math.cs
+++ Method Class Assignment/Method Class Assignment/Math.cs
@@ -2,7 +2,7 @@
 
 namespace Method_Class_Assignment {
     class Math {
-        public static void HandleMath(int int1, int int2) {
+        public void HandleMath(int int1, int int2) {
             int int1Math = int1 % 2; // calculates remainder
             Console.WriteLine(int2);
         }
--- Method Class Assignment/Method Class Assignment/Program.cs
+++ Method Class Assignment/Method Class Assignment/Program.cs
@@ -5,8 +5,9 @@ namespace Method_Class_Assignment {
         static Random random = new Random();
 
         static void Main(string[] args) {
-            Math.HandleMath(random.Next(0, 100), random.Next(0, 100)); // call method with random numbers
-            Math.HandleMath(int1: random.Next(0, 100), int2: random.Next(0, 100)); // call method with param names
+            Math math = new Math();
+            math.HandleMath(random.Next(0, 100), random.Next(0, 100)); // call method with random numbers
+            math.HandleMath(int1: random.Next(0, 100), int2: random.Next(0, 100)); // call method with param names
             Console.ReadLine(); // lets user see output
         }
     }
