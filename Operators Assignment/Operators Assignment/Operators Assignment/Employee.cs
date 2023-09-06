d4d6491bda71dac517fd5619cae6a0e202533ef9
libracucumberegg <markus.hansson04@outlook.com>
2023-09-06 14:39:33 -0700

    operator assignment

---
+++ Operators Assignment/Operators Assignment/Operators Assignment/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Operators_Assignment {
+    class Employee {
+        public int id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+
+        public Employee(int id, String FirstName, String LastName) {
+            this.id = id;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+        }
+
+        public static bool operator ==(Employee employee1, Employee employee2) { // overrides == when comparing 2 isntances of Employee
+            return employee1.id == employee2.id;
+        }
+
+        public static bool operator !=(Employee employee1, Employee employee2) { // overrides != when comparing 2 isntances of Employee
+            return employee1.id != employee2.id;
+        }
+    }
+}
---
+++ Operators Assignment/Operators Assignment/Operators Assignment/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Operators_Assignment {
+    class Program {
+        static void Main() {
+            Employee employee1 = new Employee(775930, "Stuart", "Sanchez");
+            Employee employee2 = new Employee(774938, "Walter", "White");
+
+            Console.WriteLine(employee1 == employee2);
+            Console.ReadLine();
+        }
+    }
+}
