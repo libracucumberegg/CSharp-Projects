8a912315a3ee9cf8b7581aacc88bb0f475eb6b1c
libracucumberegg <markus.hansson04@outlook.com>
2023-09-06 14:48:38 -0700

    final changes

--- Operators Assignment/Operators Assignment/Operators Assignment/Employee.cs
+++ Operators Assignment/Operators Assignment/Operators Assignment/Employee.cs
@@ -12,12 +12,21 @@ namespace Operators_Assignment {
             this.LastName = LastName;
         }
 
-        public static bool operator ==(Employee employee1, Employee employee2) { // overrides == when comparing 2 isntances of Employee
+        public static bool operator ==(Employee employee1, Employee employee2) { // overrides == when comparing 2 instances of Employee to only compare id
             return employee1.id == employee2.id;
         }
 
-        public static bool operator !=(Employee employee1, Employee employee2) { // overrides != when comparing 2 isntances of Employee
+        public static bool operator !=(Employee employee1, Employee employee2) { // overrides != when comparing 2 instances of Employee to only compare id
             return employee1.id != employee2.id;
+        }
+
+        public override bool Equals(object obj) { // overrides Equals to get Visual Studio to stop showing a warning
+            return obj is Employee employee &&
+                   id == employee.id;
+        }
+
+        public override int GetHashCode() { // overrides GetHashCode to get Visual Studio to stop showing a warning
+            return 1877310944 + id.GetHashCode();
         }
     }
 }
